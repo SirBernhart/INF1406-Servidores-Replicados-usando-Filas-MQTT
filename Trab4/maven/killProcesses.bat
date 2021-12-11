2e62494a31147a4d33f041aca0e6a14bbb364f52
SirBernhart <behornerl@gmail.com>
2021-12-11 12:00:07 -0300

    Atualiza o batch script de matar os processos de server

--- Trab4/maven/killProcesses.bat
+++ Trab4/maven/killProcesses.bat
@@ -1,4 +1,5 @@
 @echo off
 title kill java processes
-wmic Path win32_process Where "name like '%%java.exe%%' AND CommandLine like '%%Server%%'" Call Terminate
+wmic Path win32_process Where "CommandLine like '%%com.trab4.Server%%'" Call Terminate
+::wmic Path win32_process Where "name like '%%java.exe%%' AND CommandLine like '%%com.trab4.Server%%'" Call Terminate 
 pause
