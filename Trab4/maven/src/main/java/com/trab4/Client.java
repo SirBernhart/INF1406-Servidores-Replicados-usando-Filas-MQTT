b6c0879705e62760b908a063e6df3880ca52996d
SirBernhart <behornerl@gmail.com>
2021-12-11 15:51:05 -0300

    Cria classe cliente lançando requisições de inserção

---
+++ Trab4/maven/src/main/java/com/trab4/Client.java
@@ -0,0 +1,72 @@
+package com.trab4;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class Client {
+    public static void main(String[] args) {
+        String reqType = args[0];
+        String key = args[1];
+        String respTopic = args[2];
+        Integer reqId = Integer.valueOf(args[3]);
+        String newValue;
+
+        Message reqMessage;
+
+        if(reqType.equals("insert"))
+        {
+            newValue = args[4];
+            reqMessage = new Message(reqType, key, respTopic, reqId, newValue);
+        }
+        else
+        {
+            reqMessage = new Message(reqType, key, respTopic, reqId);
+        }
+
+        SendMessage(reqMessage);
+    }
+
+    private static void SendMessage(Message msg)
+    {
+        String topic        = "inf1406-reqs";
+        String content      = msg.serialize(msg);
+        int qos             = 2;
+        String broker       = "tcp://localhost:1883";
+        String clientId     = msg.getIdPedido().toString();
+        MemoryPersistence persistence = new MemoryPersistence();
+
+        try {
+            MqttClient requestingClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            requestingClient.connect(connOpts);
+            System.out.println("Client " + clientId + " ==> Publishing message: "+content);
+            MqttMessage message = new MqttMessage(content.getBytes());
+
+            message.setQos(qos);
+            requestingClient.publish(topic, message);
+
+            if(msg.getTipoMsg().equals("consult"))
+            {
+                //TODO: ficar esperando resposta
+            }
+
+            requestingClient.disconnect();
+            requestingClient.close();
+            System.out.println("Disconnected");
+            System.exit(0);
+        } 
+        catch(MqttException me) {
+            System.out.println("reason "+me.getReasonCode());
+            System.out.println("msg "+me.getMessage());
+            System.out.println("loc "+me.getLocalizedMessage());
+            System.out.println("cause "+me.getCause());
+            System.out.println("excep "+me);
+            me.printStackTrace();
+        }
+    }
+    
+}
