01ecf059a0dddebcee5bfd1d2f9242fef05ca1b5
SirBernhart <behornerl@gmail.com>
2021-12-11 12:23:31 -0300

    Atualiza script batch para iniciar o programa e depois matá-lo

--- Trab4/maven/runProgramAndKillProcesses.bat
+++ Trab4/maven/runProgramAndKillProcesses.bat
@@ -1,5 +1,7 @@
 @echo off
 title kill java processes
+start mvn compile exec:java -Dexec.mainClass="com.trab4.ServerManager" -Dexec.args="5 15"
+pause
 wmic Path win32_process Where "CommandLine like '%%com.trab4.Server%%'" Call Terminate
 ::wmic Path win32_process Where "name like '%%java.exe%%' AND CommandLine like '%%com.trab4.Server%%'" Call Terminate 
 pause
