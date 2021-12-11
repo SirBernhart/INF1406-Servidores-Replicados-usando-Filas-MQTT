d533e099ecd6dedbf73f44f9c8f74cd53433ea11
SirBernhart <behornerl@gmail.com>
2021-12-11 19:23:43 -0300

    Faz batch de request dos clientes criar 3 clientes, inserindo valores diferentes na tabela

--- Trab4/maven/sendClientsBasicRequests.bat
+++ Trab4/maven/sendClientsBasicRequests.bat
@@ -1,3 +1,5 @@
 @echo off
 title Run client basic requests
-start mvn compile exec:java -Dexec.mainClass="com.trab4.Client" -Dexec.args="insert testkey resp1 1 testvalue"
+start mvn compile exec:java -Dexec.mainClass="com.trab4.Client" -Dexec.args="insert key1 resp1 1 val1"
+start mvn compile exec:java -Dexec.mainClass="com.trab4.Client" -Dexec.args="insert key2 resp2 2 val2"
+start mvn compile exec:java -Dexec.mainClass="com.trab4.Client" -Dexec.args="insert key3 resp3 3 val3"
