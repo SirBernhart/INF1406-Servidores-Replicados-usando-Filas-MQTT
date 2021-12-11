b3158b77d666062814c5de877b289dff485742e7
SirBernhart <behornerl@gmail.com>
2021-12-11 19:23:17 -0300

    Remove pause do batch que mata os servidores

--- Trab4/maven/killServerProcesses.bat
+++ Trab4/maven/killServerProcesses.bat
@@ -2,4 +2,3 @@
 title kill server processes
 wmic Path win32_process Where "CommandLine like '%%com.trab4.Server%%'" Call Terminate
 ::wmic Path win32_process Where "name like '%%java.exe%%' AND CommandLine like '%%com.trab4.Server%%'" Call Terminate 
-pause
