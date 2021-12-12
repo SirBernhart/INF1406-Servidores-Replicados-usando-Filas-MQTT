c43b8ca47b9d11ea63a867a9858ca69a4631e0d6
SirBernhart <behornerl@gmail.com>
2021-12-11 23:43:09 -0300

    Reduz a quantidade de servidores no teste para facilitar o acompanhamento

--- Trab4/maven/initializeServers.bat
+++ Trab4/maven/initializeServers.bat
@@ -1,3 +1,3 @@
 @echo off
 title initialize servers
-start mvn compile exec:java -Dexec.mainClass="com.trab4.ServerManager" -Dexec.args="5 15"
+start mvn compile exec:java -Dexec.mainClass="com.trab4.ServerManager" -Dexec.args="3 15"
