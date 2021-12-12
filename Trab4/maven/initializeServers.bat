8b7f3b72c1b1d2cf499848b6addc1dd2fb5af25b
SirBernhart <behornerl@gmail.com>
2021-12-12 08:33:37 -0300

    Adiciona argumento de intervalo entre limpas do log para o ServerManager

--- Trab4/maven/initializeServers.bat
+++ Trab4/maven/initializeServers.bat
@@ -1,3 +1,3 @@
 @echo off
 title initialize servers
-start mvn compile exec:java -Dexec.mainClass="com.trab4.ServerManager" -Dexec.args="3 15"
+start mvn compile exec:java -Dexec.mainClass="com.trab4.ServerManager" -Dexec.args="3 30 5"
