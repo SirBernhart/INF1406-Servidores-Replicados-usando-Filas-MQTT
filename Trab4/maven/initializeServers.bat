23b0a79d833d940b46af2cfe0003b94c52cf4e35
SirBernhart <behornerl@gmail.com>
2021-12-12 21:56:22 -0300

    Script batch que cria servidores agora criando Monitor junto

--- Trab4/maven/initializeServers.bat
+++ Trab4/maven/initializeServers.bat
@@ -1,3 +1,5 @@
 @echo off
 title initialize servers
-start mvn compile exec:java -Dexec.mainClass="com.trab4.ServerManager" -Dexec.args="3 30 5"
+:: arguments: server amount / server killing interval / log cleanup interval / heartbeat interval
+start mvn compile exec:java -Dexec.mainClass="com.trab4.ServerManager" -Dexec.args="3 100 50 5"
+start mvn compile exec:java -Dexec.mainClass="com.trab4.Monitor" -Dexec.args="3 20"
